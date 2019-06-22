5ab8ef2dbf451308589918a610f48b3e9d622897
ZCYBORG-404 <51997622+ZCYBORG-404@users.noreply.github.com>
2019-06-22 20:52:34 +0800

    Add files via upload

---
+++ cek-website-ip.sh
@@ -0,0 +1,29 @@
+clear
+echo "PROMOSI DULU" | lolcat
+echo ""
+echo "===================================" | lolcat
+echo "          HELLO GUYS              " | lolcat
+echo "instagram: @everyday_meme.id.in" | lolcat
+echo "facebook: redy todek" | lolcat
+echo "github:https://github.com/ZCYBORG-404" | lolcat
+echo "YOUTUBE:KOMUNITAS Zcyb0RG" | lolcat
+echo "===================================" | lolcat
+echo ""
+echo "tools ini di buat untuk mengetahui ip website" | lolcat
+sleep 1
+
+echo ""
+echo "apa fungsi ip? ya untuk ddos lah" | lolcat
+echo "JANGAN MASUKAN URL ITU TIDAK AKAN BEKERJA" | lolcat
+echo "Contoh :www.google.com(ini benar" | lolcat
+echo "contoh :https://stackoverflow.com/(ini salah)" | lolcat
+echo "PING getjar.com (3.209.54.174) 56(84) bytes of data."
+echo "ini contoh nya     ini ip nya           " | lolcat
+echo "ngerti kan? harus ngerti dong" | lolcat
+
+sleep 10
+
+clear
+read -p "nama website target:" nama1
+ping $nama1
+CTRL+c
